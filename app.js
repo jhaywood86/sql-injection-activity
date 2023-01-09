45af56bdf494e5f48eda9cd9559df4df8f55cc87
Jamaine Haywood <105879061+jhaywood86@users.noreply.github.com>
2023-01-09 01:34:29 -0800

    javascript starter code added

---
+++ app.js
@@ -0,0 +1,40 @@
+const http = require('http'),
+path = require('path'),
+express = require('express'),
+bodyParser = require('body-parser');
+const sqlite3 = require('sqlite3').verbose();
+const app = express();
+const port = 3000
+
+app.use(express.static('.'))
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json())
+
+const db = new sqlite3.Database(':memory:');
+    db.serialize(function () {
+        db.run("CREATE TABLE user (username TEXT, password TEXT, title TEXT)");
+        db.run("INSERT INTO user VALUES ('privilegedUser', 'privilegedUser1', 'Administrator')");
+});
+
+app.get('/', (req,res)=>{
+    res.sendFile('index.html')
+})
+app.post('/login', (req,res)=>{
+    const userName=req.body.username
+    const userPass=req.body.password
+    console.log(userName, userPass)
+    const query ="SELECT title FROM User WHERE username='"+userName+"' AND password ='"+userPass+"'";
+    console.log(query)
+    db.get(query, function(err,row){
+        if(err){
+            console.log('ERROR', err)
+            res.redirect('/index.html#error')
+        } else if(!row) {
+            res.redirect('/index.html#unauthorized')
+        } else {
+            res.send('Hello <b>'+ row.title + '!</b></br> This file contains your secret data </br></br> SECRETS </br></br> MORE SECRETS </br></br><a href="/index.html">Go back to login</a>')
+
+        }
+    })
+})
+app.listen(3000)
